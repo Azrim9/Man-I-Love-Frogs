e54f3e7bfeed7112c73dcafc6af62150e3a5db7c
Azrim9 <teegeh94@gmail.com>
2025-03-13 00:28:24 -0400

    Updated Open Shop button tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -22,14 +22,17 @@ function App() {
       <div className="h-screen flex flex-col justify-between p-2">
         <div> Croak Points: {ribbitCount}</div>
         <button
-          className="border px-4 py-1 rounded-sm self-center bg-green-500 hover:bg-green-400 active:scale-95"
+          className="border px-4 py-1 rounded-sm self-center bg-green-500 hover:bg-green-400"
           onClick={() => {
             setRibbitCount(ribbitCount + 1);
           }}
         >
           Croak
         </button>
-        <button onClick={() => setIsShopOpen(!isShopOpen)}>
+        <button
+          className="border px-4 py-1 rounded-sm self-center bg-green-500 hover:bg-green-400"
+          onClick={() => setIsShopOpen(!isShopOpen)}
+        >
           {isShopOpen ? "Close Shop" : "Open Shop"}
         </button>
         {isShopOpen && <Shop buyFrog={buyFrog} />}
